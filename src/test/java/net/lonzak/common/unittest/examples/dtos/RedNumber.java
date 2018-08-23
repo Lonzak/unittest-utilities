5294f76dd53f248e05f38161b0ae90c9b542ab06
tvtreeck <tvtreeck@mail.nepatec.de>
2018-08-23 18:13:49 +0200

    upgraded version corrected some infos

--- src/test/java/net/lonzak/common/unittest/examples/dtos/RedNumber.java
+++ src/test/java/net/lonzak/common/unittest/examples/dtos/RedNumber.java
@@ -3,7 +3,7 @@
  *
  * Project Info: https://github.com/Lonzak/unittest-utilities
  * 
- * (C) Copyright 2012-2017 nepatec GmbH & Co. KG
+ * (C) Copyright 2012-2018 nepatec GmbH
  *
  * This file is part of unittest-utilities
  *
